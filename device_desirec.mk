98fe902d308d7772391b9485b69429eadf7c3bd3
bradvbos <stupiddaddy@gmail.com>
2011-04-04 15:33:41 -0400

    Eris: correct opengles # / removed a few unecessary overrides
    Change-Id: I8b6f3e3fa108c3eef1b203431439d2010c658e92

--- device_desirec.mk
+++ device_desirec.mk
@@ -110,10 +110,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # OpenGL ES 1.1-CM
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version = 65537 \
+    ro.opengles.version = 65536 \
     debug.sf.hw=1 \
-    video.accelerate.hw =1 \
-    debug.performance.tuning =1 \
     ro.product.multi_touch_enabled=true
 
 # Default network type
